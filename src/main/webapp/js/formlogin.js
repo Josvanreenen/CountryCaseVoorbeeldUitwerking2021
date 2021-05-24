c276f3643c22dfa994a86c271198a522aa96d016
Jos van Reenen <jos.vanreenen@hu.nl>
2021-05-24 23:49:37 +0200

    created form & js submit

---
+++ src/main/webapp/js/formlogin.js
@@ -0,0 +1,14 @@
+function login() {
+    var formData = new FormData(document.querySelector("#loginForm"));
+    var encData = new URLSearchParams(formData.entries());
+
+    fetch("/restservices/authentication", {method: "POST", body: encData})
+        .then(function (response) {
+            if (response.ok) return response.json(); //body will be json
+            else throw "Wrong username / password"; //there is no body, just throw the error
+        })
+        .then(myJson => window.sessionStorage.setItem("myJWT", myJson.JWT)) //present in the JsonBody
+        .catch(error => console.log(error)) //will log Wrong username/password if !response.ok
+}
+
+document.querySelector("#loginformsubmit").addEventListener("click", login);
